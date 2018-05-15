246fbfa53733f6a33f3ae32b53e0005a813c2ba5
sandeep6013 <sandeep81.srit@gmail.com>
2018-05-15 17:00:19 +0530

    PThread Creation & Tremination without pthread_exit routine

---
+++ Programming/Application/pthreads/pthread_noexit.c
@@ -0,0 +1,28 @@
+/* Purpose: PThread Creation & Tremination without pthread_exit routine
+ * Author : Sandeep Kumar M
+ */
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Thread created will execute the following routine
+ 
+void * thread_routine (void *p)
+{
+   printf("\nHello World!\n");
+}
+
+int main ()
+{
+	// Object to hold thread ID
+	pthread_t thread;
+	int rv;
+	
+	// Routine shell create a new thread
+	rv = pthread_create(&thread, NULL, thread_routine, NULL);
+	if(rv)
+		puts("Failed to create thread");
+
+	puts("Exit Main");
+}
